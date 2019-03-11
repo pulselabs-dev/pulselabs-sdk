dc349cfa39e9b67505ab8f9bfa2d582c3cf5852f
money.chaudhary <money.chaudhary11@gmail.com>
2019-03-11 18:48:34 +0530

    minor changes and added comments

--- lib/http.service.ts
+++ lib/http.service.ts
@@ -4,6 +4,10 @@ export class HttpService {
 
   }
 
+  /**
+   This method is responsible for sending all the data that we get from user to pulselabs website.
+   */
+
   postData(data: any) {
     request({
       url: 'https://http-reqbin.herokuapp.com/1bbozy51',
--- lib/pulse-labs-sdk.ts
+++ lib/pulse-labs-sdk.ts
@@ -3,32 +3,56 @@ import { CustomSkillBuilder, HandlerInput } from 'ask-sdk-core';
 import * as http from "http";
 import { RequestListener } from "http";
 
-export default class PulseLabsSdk {
+class PulseLabsSdk {
   private static _instance: PulseLabsSdk;
 
-  private httpService: HttpService;
+  private apiKey: string | null = null;
 
-  private constructor(private apiKey: string){
-    this.httpService = new HttpService();
+  private constructor(private httpService: HttpService) {
   }
+
+  /**
+   * This method is responsible for initialising apiKey and returning an object of class.
+   *
+   * Throws error if an object has already been created
+   *
+   * @param apiKey -> The apiKey allotted to the user
+   */
 
   static init(apiKey: string): PulseLabsSdk {
     if (this._instance) {
       throw new Error("Already initialised with an apiKey");
     }
-
-    this._instance = new PulseLabsSdk(apiKey);
+    // TODO:: send request to pulselabs site to verify apiKey. Once apiKey is verified create object for the class
+    this._instance = new PulseLabsSdk(new HttpService());
+    this._instance.apiKey = apiKey;
     return this._instance;
   }
 
-  attachInterceptor(skillBuilderClass: CustomSkillBuilder) {
+  /**
+   * This method is responsible for sending the request, response data for skills build using alexa Sdk
+   *
+   * Uses addResponseInterceptors method of skillBuilderClass defined by alexa
+   *
+   * @param skillBuilderClass -> the object to which the interceptor is to be attached
+   *
+   */
+
+  addAlexaInterceptor(skillBuilderClass: CustomSkillBuilder) {
     if(!this.isInitialised) {
       throw new Error("Please call init function with api key before adding interceptor for your skill");
     }
     skillBuilderClass.addResponseInterceptors(this.handleResponse.bind(this));
   }
 
-  setInterceptUrls(endPoints: string[] = []) {
+  /**
+   * This function is responsible for intercepting request , response object for self hosted node skills
+   *
+   * @param endPoints defines which routes to intercept. An empty array intercepts all the request sent on server
+   *
+   */
+
+  addNodeInterceptor(endPoints: string[] = []) {
     if(!this.isInitialised) {
       throw new Error("Please call init function with api key before adding interceptor for your skill");
     }
@@ -65,7 +89,8 @@ export default class PulseLabsSdk {
               response = JSON.parse(resBody);
               let data = {
                 request: request,
-                response: response
+                response: response,
+                skillType: 'SelfHosted'
               };
               this.httpService.postData(data);
             } catch (e) {
@@ -79,26 +104,53 @@ export default class PulseLabsSdk {
     };
   }
 
+  /**
+   * This method is called for skills generated using lambda function but without using alexa sdk
+   *
+   * Must be called every time before sending response
+   *
+   * @param request -> The request object provided by amazon server
+   *
+   * @param response -> The response object sent by the user in response to alexa request
+   *
+   */
+
   sendRequestResponseData(request: any, response: any) {
     if(!this.isInitialised) {
       throw new Error("Please call init function with api key before sending the data");
     }
     let data = {
       request: request,
-      response: response
+      response: response,
+      skillType: 'Lambda'
     };
     this.httpService.postData(data);
   }
+
+  /**
+   * This method is responsible for checking if apiKey has been set
+   *
+   */
 
   private isInitialised() {
     return !!this.apiKey;
   }
 
+  /**
+   * This method handles sending of response for skills build using alexaSdk
+   *
+   * @param handlerInput passed to the response interceptors by alexa, can be used to get the request and response object
+   *
+   */
+
   private handleResponse(handlerInput: HandlerInput) {
     let data: any = {};
     data['request'] = handlerInput.requestEnvelope;
     data['response'] = handlerInput.responseBuilder.getResponse();
+    data['skillType'] = 'AlexaSdk';
     this.httpService.postData(data);
   }
 
 }
+
+export = PulseLabsSdk;
