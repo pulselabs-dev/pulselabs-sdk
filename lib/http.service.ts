ee71bc5b2aca8803564abff7eef3a1f1d0f70f10
money.chaudhary <money.chaudhary11@gmail.com>
2019-03-25 18:34:34 +0530

    changed req bin url

--- lib/http.service.ts
+++ lib/http.service.ts
@@ -11,7 +11,7 @@ export class HttpService {
   postData(data: any) : Promise<any> {
     return new Promise((resolve, reject) => {
       request.post({
-        url: 'https://http-reqbin.herokuapp.com/y32kxpy3',
+        url: 'https://http-reqbin.herokuapp.com/w7jjc9w7',
         json: true,
         body: data
       }, (error, response, body) => {
