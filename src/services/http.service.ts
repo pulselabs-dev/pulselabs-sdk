585c035c69b4099a81b8ec584d2474b1405cdbb7
Archit Garg <thearchitgargg@gmail.com>
2020-02-14 12:37:34 +0530

    handle error code nicely

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -13,8 +13,8 @@ export class HttpService {
    This method is responsible for sending all the data that we get from user to pulselabs website.
    */
 
-  postData(data: ServerData, platform: Platform) : Promise<any> {
-    return fetch(`https://sdkapi.pulselabs.ai/api/sdk/v1/conversations/${platform}`, {
+  async postData(data: ServerData, platform: Platform) : Promise<any> {
+    const fetchResult = await fetch(`https://sdkapi.pulselabs.ai/api/sdk/v1/conversations/${platform}`, {
       method: "POST",
       headers: {
         'Accept': 'application/json',
@@ -22,8 +22,14 @@ export class HttpService {
       },
       body: JSON.stringify(data),
       timeout: this.configService.timeout
-    }).catch(error => {
-      throw JSON.stringify(error.body);
     });
+
+    const jsonResponse = await fetchResult.json();
+
+    if (fetchResult.ok) {
+      return jsonResponse;
+    } else {
+      throw jsonResponse
+    }
   }
 }
